08d862a74755ee8636d890d14a8ed8e67add1114
maxshq <maxshqha@gmail.com>
2023-05-28 20:22:38 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,63 +1,53 @@
 const { app, BrowserWindow } = require('electron')
-const path = require("path")
-const gotTheLock = app.requestSingleInstanceLock()
-require('update-electron-app')()
 let win
-function createWindow() {
-    win = new BrowserWindow({
-        width: 1420, height: 800, icon: path.resolve(__dirname, "./gkstudycourse.jpg"),
-        webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: true,
-            enableRemoteModule: true,
-            webSecurity: true,
-            nodeIntegrationInSubFrames: true,
-            nodeIntegrationInWorker: true,
-            preload: path.resolve(__dirname, "preload.js"),
-            partition: String(+new Date()),
-        }
-    })
-    win.loadURL("https://menhu.pt.ouchn.cn/site/ouchnPc/index",
-        {
-            userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
-        }
-    )
-    // win.webContents.closeDevTools()
-    win.webContents.openDevTools()
-    win.once('ready-to-show', () => {
-        win.show();
-    })
+
+function createWindow () {
+  win = new BrowserWindow({
+    width: 1800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      webviewTag: true
+    }
+  })
+
+  win.loadFile('index.html')
+
+  const webview = document.createElement('webview')
+  webview.src = '<https://menhu.pt.ouchn.cn/site/ouchnPc/index>'
+  webview.addEventListener('dom-ready', () => {
+    webview.executeJavaScript(`alert(document.title)`)
+  })
+
+  win.webContents.on('did-finish-load', () => {
+    win.webContents.executeJavaScript(`
+      const webview = document.querySelector('webview')
+      webview.addEventListener('dom-ready', () => {
+        webview.executeJavaScript(\\`
+          const body = document.querySelector('body')
+          const p = document.createElement('p')
+          p.innerText = 'Hello from Electron'
+          body.appendChild(p)
+        \\`)
+      })
+    `)
+  })
+
+  win.on('closed', () => {
+    win = null
+  })
 }
-if (!gotTheLock) {
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit()
-} else {
-    app.on('second-instance', (event, commandLine, workingDirectory) => {
-        // 用户正在尝试运行第二个实例，我们需要让焦点指向我们的窗口
-        if (mainWindow) {
-            if (mainWindow.isMinimized()) mainWindow.restore()
-            mainWindow.focus()
-        }
-    })
-    app.on("web-contents-created", (event, w) => {
-        w.setWindowOpenHandler((details) => {
-            win.loadURL(details.url, { userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36" })
-            return { "action": "deny" }
-        })
-    })
+  }
+})
 
-    app.whenReady().then(() => {
-        createWindow()
-        // win.webContents.on('did-finish-load', () => {
-        //     win.webContents.send("load", { "win": win.webContents });
-
-        // })
-        app.on('activate', function () {
-            if (win || BrowserWindow.getAllWindows().length === 0) createWindow()
-        })
-    })
-    app.on('window-all-closed', () => {
-        if (process.platform !== 'darwin') {
-            app.quit()
-        }
-    })
-}
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
